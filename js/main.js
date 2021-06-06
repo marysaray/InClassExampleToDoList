5fbc873f40b0389fc9d4573133edea7bfb5294f6
Mary Saray <msaray.ms@outlook.com>
2021-06-05 18:39:00 -0700

    Created web page and added js-datepicker.

---
+++ js/main.js
@@ -0,0 +1,4 @@
+var mainDate = datepicker("#date");
+mainDate.setMin(new Date());
+var dueDate = datepicker("#due-date");
+dueDate.setMin(new Date());
---
+++ ts/main.ts
@@ -0,0 +1,6 @@
+// @ts-ignore -- no intellisense for js-datepicker.
+const mainDate = datepicker("#date");
+mainDate.setMin(new Date()); // set today's date
+// @ts-ignore -- no intellisense for js-datepicker.
+const dueDate = datepicker("#due-date");
+dueDate.setMin(new Date());
